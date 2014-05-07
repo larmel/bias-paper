f8f93db69cdfd1f471c008775faad47e80a0c1ed
Lars Kirkholt Melhus <lars.kirkholt.melhus@gmail.com>
2014-05-07 21:43:37 +0200

    Update pgf converter

--- util/pgfpconv.py
+++ util/pgfpconv.py
@@ -1,22 +1,28 @@
 #!/usr/bin/env python
 import sys
 
+# Convert output of lperf to a format that is easier to parse for the LaTeX 
+# pgfplots package.
 #
-# Script to convert output of lperf to a format that is easier to parse for
-# the LaTeX pgfplots package. 
+# From: [name, mnemonic {,correlation}?, 0, 1, 2, ...]
+#
+# To:     n     cycles:u    r0107:u   ...
+#         0        50          8
+#         1         6          1
+#
 # Usage: lperf <args> | pgfconv > output.dat
-
+#
 if __name__ == '__main__':
-	data = [[]]
-	for line in sys.stdin:
-		if line[0] == '#':
-			sys.stdout.write(line)
-		else:
-			row = line.strip().split(',')
-			header, numbers = row[1], row[3:]
-			data.append([header] + numbers)
+    data = [[]]
+    header = sys.stdin.readline()
+    startidx = header.split(',').index('0')
+    
+    for line in sys.stdin:
+        row = line.strip().split(',')
+        event, numbers = row[1], row[startidx:]
+        data.append([event] + numbers)
 
-	n = len(data[1])
-	data[0] = ["n"] + map(str, range(0, n - 1))
-	for i in range(n):
-		print '\t'.join(data[k][i] for k in range(len(data)))
+    n = len(data[1])
+    data[0] = ["n"] + map(str, range(0, n - 1))
+    for i in range(n):
+        print '\t'.join(data[k][i] for k in range(len(data)))
