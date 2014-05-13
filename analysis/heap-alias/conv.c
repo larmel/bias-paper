e93109ac075e78f9fc7008f09c40c020153baf19
Lars Kirkholt Melhus <lars.kirkholt.melhus@gmail.com>
2014-05-13 21:12:48 +0200

    Add O2 targets and set const input buffer default

--- analysis/heap-alias/conv.c
+++ analysis/heap-alias/conv.c
@@ -1,6 +1,6 @@
 static float kernel[5] = {0.1, 0.25, 0.3, 0.25, 0.1};
 
-void convolve(int size, float *input, float *output)
+void convolve(int size, const float *input, float *output)
 {
     int i, j;
     for (i = 2; i < size - 2; ++i)
