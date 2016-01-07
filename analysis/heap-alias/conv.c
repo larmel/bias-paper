c61b441e68555648a82edb3f398fbe10fc482be4
Rune E. Jensen <runeerle@stud.ntnu.no>
2016-01-07 15:09:32 +0100

    Compacted the code a bit for the paper.

--- analysis/heap-alias/conv.c
+++ analysis/heap-alias/conv.c
@@ -1,12 +1,10 @@
-static float k[5] = {0.1, 0.25, 0.3, 0.25, 0.1};
-
-void conv(int n, const float *input, float *output)
+static float k[5]= {0.1, 0.25, 0.3, 0.25, 0.1};
+void conv(int n, const float *in, float *out)
 {
     int i, j;
-    for (i = 2; i < n - 2; ++i)
-    {
-        output[i] = 0;
+    for (i = 2; i < n - 2; ++i) {
+        out[i] = 0;
         for (j = 0; j < 5; ++j)
-            output[i] += input[i-2+j] * k[j];
+            out[i] += in[i-2+j] * k[j];
     }
 }
