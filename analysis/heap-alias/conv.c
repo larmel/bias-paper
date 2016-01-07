83634ab2f3848eb04fd5a35367da9c815622d405
Lars Kirkholt Melhus <lars.kirkholt.melhus@gmail.com>
2016-01-07 19:43:52 +0100

    Make code compacting change consistent

--- analysis/heap-alias/conv.c
+++ analysis/heap-alias/conv.c
@@ -1,8 +1,9 @@
 static float k[5]= {0.1, 0.25, 0.3, 0.25, 0.1};
 void conv(int n, const float *in, float *out) {
-    for (int i = 2; i < n - 2; ++i) {
+    int i, j;
+    for (i = 2; i < n - 2; ++i) {
         out[i] = 0;
-        for (int j = 0; j < 5; ++j)
+        for (j = 0; j < 5; ++j)
             out[i] += in[i-2+j] * k[j];
     }
 }
