2861f119678d0973b3a4dde957ac43f7b433dc82
Lars Kirkholt Melhus <lars.kirkholt.melhus@gmail.com>
2016-03-06 13:39:51 +0100

    Adjust formatting to fit better

--- analysis/heap-alias/conv.c
+++ analysis/heap-alias/conv.c
@@ -1,9 +1,11 @@
-static float k[5]= {0.1, 0.25, 0.3, 0.25, 0.1};
+static float k[5] = {0.1, 0.25, 0.3, 0.25, 0.1};
+
 void conv(int n, const float *in, float *out) {
     int i, j;
     for (i = 2; i < n - 2; ++i) {
         out[i] = 0;
-        for (j = 0; j < 5; ++j)
+        for (j = 0; j < 5; ++j) {
             out[i] += in[i-2+j] * k[j];
+        }
     }
 }
